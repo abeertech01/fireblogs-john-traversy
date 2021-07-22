88051ece0ab9b1f4e94b27f0dcd6366737a58101
Abeer <abeer.technology@gmail.com>
2021-07-22 12:50:47 +0600

    completed the project setup part and added doc file

---
+++ DOC.md
@@ -0,0 +1,7 @@
+# Project Setup
+First of all set a project in firebase. then created a web app in firebase project. From that web app I took the firebaseConfig variable and brought it into the firebaseInit file inside firebase folder.
+
+1 = here I don't need all of the firebase services. I just need the app services. that's why i imported only the app service.
+2 = here I imported the firestore service.
+3 = initialized firebase app
+4 = firebase timestamp. now i may not understand what is the timestamp, but later he will explain.
--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/app"; // [1]
+import "firebase/firestore"; // [2]
 
 var firebaseConfig = {
   apiKey: "AIzaSyAKnUh4ef9jpgFZOwtAiRaFXQwsZafH0bk",
@@ -10,8 +10,8 @@ var firebaseConfig = {
   appId: "1:249604017814:web:119a9a53bcaee803a5bcb7"
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+const firebaseApp = firebase.initializeApp(firebaseConfig); // [3]
+const timestamp = firebase.firestore.FieldValue.serverTimestamp; // [4]
 
 export { timestamp };
 export default firebaseApp.firestore();
